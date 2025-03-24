f02cb57671d6d2caad04dde78ddc5e2757b6b699
Nikhil <nikhil.bhosle@atriina.com>
2025-03-24 12:51:26 +0530

    NIKHIL :: Remove Datavase connecrion

--- drizzle-kit/meetzydb.drizzle.config.ts
+++ drizzle-kit/meetzydb.drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     out: "./src/dbmodels/drizzel/meetzydb/migrations",
     dialect: "postgresql", // "postgresql" | "mysql"
     dbCredentials: {
-        url: "postgresql://pguser:R5sWDsMWc7aYHfQc@ec2-13-235-15-173.ap-south-1.compute.amazonaws.com:5432/MeetZyDBDev"
+        url: ""
     },
     introspect: {
         casing: 'preserve'
